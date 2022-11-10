4ec3594d89ddcd74ee9e6e6548c652590ef32d2d
midrain1102 <midrain1102@gmail.com>
2022-11-10 22:47:13 +0700

    create Areas

---
+++ 2001207118_NguyenNgocThien/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using _2001207118_NguyenNgocThien.Models;
+
+namespace _2001207118_NguyenNgocThien.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin/Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 2001207118_NguyenNgocThien/Controllers/AccountController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using _2001207118_NguyenNgocThien.Models;
+using _2001207118_NguyenNgocThien.ViewModel;
+using _2001207118_NguyenNgocThien.Identity;
+using System.Web.Helpers;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace _2001207118_NguyenNgocThien.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(RegisterVM rvm)
+        {
+            if (ModelState.IsValid)
+            {
+                var appDbContext = new AppDbContext();
+                var userStore = new AppUserStore(appDbContext);
+                var userManager = new AppUserManager(userStore);
+                var passwdHash = Crypto.HashPassword(rvm.Password);
+                var user = new AppUser()
+                {
+                    Email = rvm.Email,
+                    UserName = rvm.Username,
+                    PasswordHash = passwdHash,
+                    City = rvm.City,
+                    BirthDay = rvm.DateOfBirth,
+                    Address = rvm.Address,
+                    PhoneNumber = rvm.Mobile
+                };
+                IdentityResult identityResult = userManager.Create(user);
+                if (identityResult.Succeeded)
+                {
+                    userManager.AddToRole(user.Id, "Customer");
+
+                    var authenManager = HttpContext.GetOwinContext().Authentication;
+                    var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                    authenManager.SignIn(new AuthenticationProperties(), userIdentity);
+                }
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("New Error", "Invalid Data");
+                return View();
+            }
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginVM lvm)
+        {
+            var appDbContext = new AppDbContext();
+            var userStore = new AppUserStore(appDbContext);
+            var userManager = new AppUserManager(userStore);
+            var user = userManager.Find(lvm.Username, lvm.Password);
+            if (user != null)
+            {
+                var authenManager = HttpContext.GetOwinContext().Authentication;
+                var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+                authenManager.SignIn(new AuthenticationProperties(), userIdentity);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("MyError", "Invalid username and password");
+                return View();
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            var authenManager = HttpContext.GetOwinContext().Authentication;
+            authenManager.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
